bfd9856370bf93241e7af6ef14e0d669c1cce1e8
galofazio <galofazio@gmail.com>
2023-10-09 16:59:13 -0300

    little changes in the css and first idea of login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AsideComponent } from './aside/aside.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AsideComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
