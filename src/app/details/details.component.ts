a3cd0ae10571791a34924fcdaa46eb019ab8e55e
Tbuixados <buixadostomas@gmail.com>
2023-11-07 14:29:26 -0300

    transactions filter

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -5,6 +5,9 @@ import { Transaction } from '../login/user.transaction';
 import { UserVerificationService } from '../user-verification.service';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
+import { NgModule } from '@angular/core';
+import * as moment from 'moment';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -21,6 +24,12 @@ export class DetailsComponent {
   incomeList: Transaction[] = [];
   outcomeList: Transaction[] = [];
 
+  filterDescription: string = '';
+  filterStartDate: Date | null = null;
+  filterEndDate: Date | null = null;
+
+  originalTransactions: Transaction[] = [];
+
   constructor(
     private userVariable: UserVariableService,
     private userVerification: UserVerificationService
@@ -34,8 +43,16 @@ export class DetailsComponent {
         this.outcomeList.push(t);
       }
     });
+
+    this.originalTransactions = [...this.user.transactions!];
+
+
     console.log(this.user.preferredCurrency);
     this.isData = true;
+  }
+
+  ngOnDestroy() {
+    this.user.transactions = [...this.originalTransactions];
   }
 
   clickOnIncome() {
@@ -66,21 +83,28 @@ export class DetailsComponent {
     this.clickedIndex = index === this.clickedIndex ? -1 : index;
   }
 
- 
   exportToPDF() {
     const doc = new jsPDF();
 
-    let transactionsArray = [...this.user.transactions!]
-    let totalAmount = this.calculateTotal()
-    let operator!: string
-    if(totalAmount>=0){
-      operator = '+'
-    }else{
-      operator = '-'
-      totalAmount = Math.abs(totalAmount)
+    let transactionsArray = [...this.user.transactions!];
+    let totalAmount = this.calculateTotal();
+    let operator!: string;
+    if (totalAmount >= 0) {
+      operator = '+';
+    } else {
+      operator = '-';
+      totalAmount = Math.abs(totalAmount);
     }
-    let totalTransactions = new Transaction('', 'TOTAL', this.calculateTotal(), '', '', operator, '')
-    transactionsArray?.push(totalTransactions)
+    let totalTransactions = new Transaction(
+      '',
+      'TOTAL',
+      this.calculateTotal(),
+      '',
+      '',
+      operator,
+      ''
+    );
+    transactionsArray?.push(totalTransactions);
 
     const orderedData = transactionsArray!.map((transaction) => [
       transaction.date!,
@@ -95,38 +119,89 @@ export class DetailsComponent {
     logoImg.src = './../../assets/icons/greenLogoLarge.png';
 
     logoImg.onload = function () {
-    doc.addImage(logoImg, 'PNG', 14, 2, 30, 10);
+      doc.addImage(logoImg, 'PNG', 14, 2, 30, 10);
 
-    autoTable(doc, {
-      head: [
-        ['Date', 'Description', 'Category', 'Participant', 'Type', 'Amount'],
-      ],
-      body: orderedData,
-      headStyles: {
-        fillColor: [134, 195, 50], 
-        textColor: [0, 0, 0] 
-      }
-    });
+      autoTable(doc, {
+        head: [
+          ['Date', 'Description', 'Category', 'Participant', 'Type', 'Amount'],
+        ],
+        body: orderedData,
+        headStyles: {
+          fillColor: [134, 195, 50],
+          textColor: [0, 0, 0],
+        },
+      });
 
-    doc.save('Transactions.pdf');
-    }
+      doc.save('Transactions.pdf');
+    };
 
-    transactionsArray = []
+    transactionsArray = [];
   }
 
-
-  calculateTotal(){
-    let total = 0
-    this.user.transactions!.forEach(element => {
-      if(element.type === "income"){
-        total += element.amount!
-        console.log('income')
-      }else if(element.type === "outcome"){
-        total -= element.amount!
-        console.log('outcome')
+  calculateTotal() {
+    let total = 0;
+    this.user.transactions!.forEach((element) => {
+      if (element.type === 'income') {
+        total += element.amount!;
+        console.log('income');
+      } else if (element.type === 'outcome') {
+        total -= element.amount!;
+        console.log('outcome');
       }
     });
 
-    return total
+    return total;
+  }
+
+  applyFilters() {
+    this.user.transactions = [...this.incomeList, ...this.outcomeList];
+    this.user.transactions = this.user.transactions!.filter((t) =>
+      t.description
+        ?.toLowerCase()
+        .includes(this.filterDescription.toLowerCase())
+    );
+
+    if (this.filterStartDate && this.filterEndDate) {
+      let startDate = moment(this.filterStartDate);
+      let stringStartDate = this.concatenateDate(startDate);
+      let endDate = moment(this.filterEndDate);
+      let stringEndDate = this.concatenateDate(endDate);
+      this.user.transactions = this.user.transactions.filter((t) => {
+        const transactionDate = moment(t.date, 'YYYY-MM-DD'); // AsegÃºrate de especificar el formato de fecha correcto
+
+        return (
+          transactionDate.isSameOrAfter(stringStartDate, 'day') &&
+          transactionDate.isSameOrBefore(stringEndDate, 'day')
+        );
+      });
+    }
+  }
+  concatenateDate(date: any) {
+    let day = date.get('date');
+    let dayS = day.toString();
+    if (dayS.length === 1) {
+      dayS = '0' + dayS;
+    }
+
+    let month = date.get('month') + 1;
+    let monthS = month.toString();
+    if (monthS.length === 1) {
+      monthS = '0' + monthS;
+    }
+
+    let year = date.get('year');
+    let yearS = year.toString();
+
+    let d = yearS + '-' + monthS + '-' + dayS;
+
+    return d;
+  }
+
+  resetFilters() {
+    this.user.transactions = [...this.incomeList, ...this.outcomeList];
+
+    this.filterDescription = '';
+    this.filterStartDate = null;
+    this.filterEndDate = null;
   }
 }
